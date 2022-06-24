5c8bf3751be439a18c50b211528e2016edfeb997
Abel0Kassahun <abellakasuuju6624@gmail.com>
2022-06-25 00:27:50 +0300

    changed the way i casted the variables

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -14,7 +14,7 @@ namespace GradeCalculator
             Console.WriteLine("How many students grades would you like to calculate?");
             do
             {
-                numberOfGrades = int.Parse(Console.ReadLine());
+                numberOfGrades = Convert.ToInt32(Console.ReadLine());
                 if (numberOfGrades <= 0)
                 {
                     Console.WriteLine("Your input should be greater than 0, try again");
@@ -27,7 +27,7 @@ namespace GradeCalculator
                 do
                 {
                     checker = true;
-                    grade = Double.Parse(Console.ReadLine());
+                    grade = Convert.ToDouble(Console.ReadLine());
                     if (grade < 0.0 || grade > 100.0)
                     {
                         Console.WriteLine("Your input grade should be between 0 and 100, Try again");
@@ -35,39 +35,39 @@ namespace GradeCalculator
                     }
                 } while (!checker);
 
-                if (grade <= 100 && grade >= 90)
+                if (grade <= 100.0 && grade >= 90.0)
                 {
                     lettergrade = "A+";
                 }
-                else if (grade < 90 && grade >= 80)
+                else if (grade < 90.0 && grade >= 80.0)
                 {
                     lettergrade = "A";
                 }
-                else if (grade < 80 && grade >= 75)
+                else if (grade < 80.0 && grade >= 75.0)
                 {
                     lettergrade = "B+";
                 }
-                else if (grade < 75 && grade >= 70)
+                else if (grade < 75.0 && grade >= 70.0)
                 {
                     lettergrade = "B";
                 }
-                else if (grade < 70 && grade >= 65)
+                else if (grade < 70.0 && grade >= 65.0)
                 {
                     lettergrade = "C+";
                 }
-                else if (grade < 65 && grade >= 60)
+                else if (grade < 65.0 && grade >= 60.0)
                 {
                     lettergrade = "C";
                 }
-                else if (grade < 60 && grade >= 55)
+                else if (grade < 60.0 && grade >= 55.0)
                 {
                     lettergrade = "D+";
                 }
-                else if (grade < 55 && grade >= 50)
+                else if (grade < 55.0 && grade >= 50.0)
                 {
                     lettergrade = "D";
                 }
-                else if (grade < 50)
+                else if (grade < 50.0)
                 {
                     lettergrade = "F";
                 }
